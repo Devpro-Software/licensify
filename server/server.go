1ab629ee5c2cd73c1001cca4806cd5768097fe03
Gianluca Piccirillo <gianluca.piccirillo@mail.mcgill.ca>
2025-03-27 22:58:41 -0400

    start portal

--- server/server.go
+++ server/server.go
@@ -4,9 +4,14 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
+	"os"
+	"path/filepath"
+	"time"
 
 	"github.com/Devpro-Software/licensify/licensify"
+	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -63,142 +68,169 @@ func (s *Server) Start() {
 	}
 
 	s.db.AutoMigrate(&License{})
-	s.setup()
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", s.port), nil))
+	g := gin.Default()
+	s.setup(g)
+	g.Run(fmt.Sprintf(":%s", s.port))
 }
 
-func (s *Server) setup() {
-	auth := func(next http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			apikey := r.Header.Get("API-KEY")
-			if apikey != s.apiKey {
-				http.Error(w, "", http.StatusUnauthorized)
-				return
-			}
+func generateErrorCode() string {
+	return fmt.Sprintf("ERR-%d-%d", time.Now().UnixNano(), rand.Intn(1000))
+}
 
-			next.ServeHTTP(w, r)
-		})
+func internalError(c *gin.Context, err error) {
+	code := generateErrorCode()
+	c.Error(fmt.Errorf("%s - %v", code, err))
+	c.JSON(http.StatusInternalServerError, gin.H{"error": code})
+}
+
+func getApiKey(c *gin.Context) string {
+	const apiKeyHeader = "API-KEY"
+	return c.GetHeader(apiKeyHeader)
+}
+
+func (s *Server) securityFilter(c *gin.Context) {
+	apiKey := getApiKey(c)
+	if apiKey == "" {
+		c.Status(http.StatusUnauthorized)
+		c.Abort()
+		return
 	}
 
-	http.Handle("/licenses", auth(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		switch r.Method {
-		case "GET":
-			var licenses []*License
-			if err := s.db.Find(&licenses).Error; err != nil {
-				http.Error(w, "Internal error", http.StatusInternalServerError)
-				log.Println(err)
-				return
-			}
+	if s.apiKey != apiKey {
+		c.Status(http.StatusUnauthorized)
+		c.Abort()
+		return
+	}
+	c.Next()
+}
 
-			json.NewEncoder(w).Encode(&licenses)
-		case "POST":
-			var req struct {
-				Product string         `json:"product"`
-				Data    map[string]any `json:"data"`
-				Active  bool           `json:"active"`
-			}
-
-			if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
-				http.Error(w, "Invalid body", http.StatusBadRequest)
-				return
-			}
-			defer r.Body.Close()
-
-			license := License{}
-			license.ID = uuid.New().String()
-			license.Active = req.Active
-			license.Product = req.Product
-			license.Data = req.Data
-
-			if err := s.db.Create(&license).Error; err != nil {
-				http.Error(w, "Internal error", http.StatusInternalServerError)
-				return
-			}
-
-			json.NewEncoder(w).Encode(license)
-		}
-	})))
-
-	http.Handle("/licenses/{id}", auth(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		licenseID := r.PathValue("id")
-		var license License
-		if s.db.First(&license, "id = ?", licenseID).Error != nil {
-			http.NotFound(w, r)
+func (s *Server) setup(g *gin.Engine) {
+	gr := g.Group("/api")
+	gr.Use(s.securityFilter)
+	gr.GET("/licenses", func(ctx *gin.Context) {
+		var licenses []*License
+		if err := s.db.Find(&licenses).Error; err != nil {
+			internalError(ctx, err)
 			return
 		}
 
-		switch r.Method {
-		case "GET":
-			json.NewEncoder(w).Encode(&license)
-		case "PUT":
-			active := r.URL.Query().Get("active")
-			switch active {
-			case "true":
-				license.Active = true
-				s.db.Save(&license)
-			case "false":
-				license.Active = false
-				s.db.Save(&license)
-			}
+		ctx.JSON(http.StatusOK, &licenses)
+	})
 
-			json.NewEncoder(w).Encode(&license)
+	gr.POST("/licenses", func(ctx *gin.Context) {
+		var req struct {
+			Product string         `json:"product"`
+			Data    map[string]any `json:"data"`
+			Active  bool           `json:"active"`
 		}
-	})))
 
-	http.Handle("/licenses/{id}/sign", auth(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		licenseID := r.PathValue("id")
-		var license License
-		if s.db.First(&license, "id = ?", licenseID).Error != nil {
-			http.NotFound(w, r)
+		if err := json.NewDecoder(ctx.Request.Body).Decode(&req); err != nil {
+			http.Error(ctx.Writer, "Invalid body", http.StatusBadRequest)
+			return
+		}
+		defer ctx.Request.Body.Close()
+
+		license := License{}
+		license.ID = uuid.New().String()
+		license.Active = req.Active
+		license.Product = req.Product
+		license.Data = req.Data
+
+		if err := s.db.Create(&license).Error; err != nil {
+			internalError(ctx, err)
 			return
 		}
 
-		switch r.Method {
-		case "POST":
-			sig, err := s.signer.Sign(licensify.NewLicense(map[string]string{
-				"license-id": license.ID,
-				"product":    license.Product,
-			}))
-			if err != nil {
-				http.Error(w, "Internal error", http.StatusInternalServerError)
-				log.Println(err)
-				return
-			}
+		ctx.JSON(http.StatusOK, license)
+	})
 
-			json.NewEncoder(w).Encode(&sig)
-		default:
-			http.NotFound(w, r)
-
+	gr.GET("/licenses/:id", func(ctx *gin.Context) {
+		licenseID := ctx.Param("id")
+		var license License
+		if s.db.First(&license, "id = ?", licenseID).Error != nil {
+			http.NotFound(ctx.Writer, ctx.Request)
+			return
 		}
-	})))
 
-	http.HandleFunc("/validate", func(w http.ResponseWriter, r *http.Request) {
+		ctx.JSON(http.StatusOK, &license)
+	})
+
+	gr.PUT("/licenses/:id", func(ctx *gin.Context) {
+		licenseID := ctx.Param("id")
+		var license License
+		if s.db.First(&license, "id = ?", licenseID).Error != nil {
+			http.NotFound(ctx.Writer, ctx.Request)
+			return
+		}
+
+		active := ctx.Request.URL.Query().Get("active")
+		switch active {
+		case "true":
+			license.Active = true
+			s.db.Save(&license)
+		case "false":
+			license.Active = false
+			s.db.Save(&license)
+		}
+
+		ctx.JSON(http.StatusOK, &license)
+	})
+
+	gr.POST("/licenses/:id/sign", func(ctx *gin.Context) {
+		licenseID := ctx.Param("id")
+		var license License
+		if s.db.First(&license, "id = ?", licenseID).Error != nil {
+			http.NotFound(ctx.Writer, ctx.Request)
+			return
+		}
+		sig, err := s.signer.Sign(licensify.NewLicense(map[string]string{
+			"license-id": license.ID,
+			"product":    license.Product,
+		}))
+		if err != nil {
+			internalError(ctx, err)
+			return
+		}
+
+		ctx.JSON(http.StatusOK, &sig)
+	})
+
+	gr.POST("/validate", func(ctx *gin.Context) {
 		var sig licensify.Signature
-		if err := json.NewDecoder(r.Body).Decode(&sig); err != nil {
-			http.Error(w, "Invalid request body - expected a signature", http.StatusBadRequest)
+		if err := json.NewDecoder(ctx.Request.Body).Decode(&sig); err != nil {
+			internalError(ctx, err)
 			return
 		}
-		defer r.Body.Close()
+		defer ctx.Request.Body.Close()
 
 		err := s.verifier.Verify(&sig)
 		if err != nil {
-			http.Error(w, "Invalid signature", http.StatusUnauthorized)
+			http.Error(ctx.Writer, "Invalid signature", http.StatusUnauthorized)
 			return
 		}
 
 		licenseID := sig.License["license-id"]
 		var license License
 		if err := s.db.First(&license, "id = ?", licenseID).Error; err != nil {
-			http.Error(w, "Internal error", http.StatusInternalServerError)
-			log.Println(err)
+			internalError(ctx, err)
 			return
 		}
 
 		if !license.Active {
-			http.Error(w, "License Inactive", http.StatusUnauthorized)
+			http.Error(ctx.Writer, "License Inactive", http.StatusUnauthorized)
 			return
 		}
 
-		w.WriteHeader(http.StatusOK)
+		ctx.Status(http.StatusOK)
+	})
+
+	g.NoRoute(func(ctx *gin.Context) {
+		log.Println(ctx.Request.URL.Path)
+		dir := http.Dir("client/out")
+		_, err := os.Stat(filepath.Join("client", "out", ctx.Request.URL.Path+".html"))
+		if err == nil {
+			ctx.Request.URL.Path = ctx.Request.URL.Path + ".html"
+		}
+		http.FileServer(dir).ServeHTTP(ctx.Writer, ctx.Request)
 	})
 }
